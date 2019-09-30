39e01f3ab24a2659473381da5046aa07c91599bb
项一繁 <yifan@xiangyifandeMac-mini.local>
2019-09-30 11:01:37 +0800

    init 🍐

---
+++ gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `Stutter`,
+    author: `My Name`,
+    description: `My site description...`,
+    social: [
+      {
+        name: `github`,
+        url: `https://github.com/gatsbyjs`,
+      },
+    ],
+  },
+}

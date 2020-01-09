436be1214ff5b582cb11dd2267026128673c5cd4
diliburong <diliburong@126.com>
2020-01-09 13:50:09 +0800

    add ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,13 @@ module.exports = {
         ]
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-123575007-2",
+      },
+    },
   ],
   // Customize your site metadata:
   siteMetadata: {
